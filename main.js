175a7a9e69dbd6632b5e460549f3648ce345f411
tegarfadillah-myz <tegarfadila2233@gmail.com>
2025-02-05 00:56:58 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+onload = () =>{
+    const c = setTimeout(() => {
+        document.body.classList.remove("container");
+    
+        const titles = ('i love you zuka sayang, maafin aku yaa yang selalu buat masalah ini, say that you love me too').split('')
+        const titleElement = document.getElementById('title');
+        let index = 0;
+    
+        function appendTitle() {
+            if (index < titles.length) {
+            titleElement.innerHTML += titles[index];
+            index++;
+            setTimeout(appendTitle, 300); // 1000ms delay
+            }
+        }
+    
+        appendTitle();
+    
+        clearTimeout(c);
+        }, 1000);
+};
